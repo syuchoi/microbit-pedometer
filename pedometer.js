1a9ee80f002659be75ff3f49c2e00ac4c70a9dfb
Choi Sung Il <93532709+syuchoi@users.noreply.github.com>
2023-05-02 16:10:19 +0900

    Update pedometer.js

--- pedometer.js
+++ pedometer.js
@@ -1,11 +1,52 @@
 input.onButtonPressed(Button.A, function () {
-    basic.clearScreen()
-    stepCount = 0
+    if (minimode == true) {
+        minimode = false
+    } else {
+        minimode = true
+    }
+    printLED()
 })
 input.onGesture(Gesture.Shake, function () {
     stepCount += 1
-    if (stepCount == 10 || stepCount == 100 || stepCount == 1000 || stepCount == 10000) {
+    // Fix lag issues
+    if (stepCount < 10 && ledON == true) {
+        printLED()
+    }
+    if (stepCount > 1 && Math.log(stepCount) / Math.log(10) % 1 == 0 && stepCount <= 10000) {
         melody()
+    }
+})
+function printLED () {
+    basic.clearScreen()
+    if (minimode == false) {
+        basic.showNumber(stepCount)
+    } else {
+        if (stepCount % 2 == 0) {
+            basic.showIcon(IconNames.SmallSquare)
+        } else {
+            basic.showIcon(IconNames.Square)
+        }
+    }
+}
+input.onButtonPressed(Button.AB, function () {
+    basic.clearScreen()
+    stepCount = 0
+    // Fix lag issues
+    if (ledON == true && minimode == false) {
+        basic.showNumber(stepCount)
+    } else {
+        basic.showIcon(IconNames.Yes)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (ledON == true) {
+        basic.clearScreen()
+        ledON = false
+    } else {
+        ledON = true
+    }
+    if (stepCount < 10 && ledON == true) {
+        printLED()
     }
 })
 function melody () {
@@ -21,8 +62,17 @@ function melody () {
     music.rest(music.beat(BeatFraction.Eighth))
     music.playTone(784, music.beat(BeatFraction.Whole))
 }
+let minimode = false
+let ledON = false
 let stepCount = 0
 music.setTempo(134)
+basic.showNumber(stepCount)
+ledON = true
+minimode = false
 basic.forever(function () {
-    basic.showNumber(stepCount)
+    if (stepCount >= 10 && ledON == true) {
+        printLED()
+    } else if (stepCount >= 10 && ledON == false) {
+        basic.clearScreen()
+    }
 })
